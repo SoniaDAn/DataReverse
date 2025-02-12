5a3b3d9e5f67d65b25db10d9a85ccf23365a4b75
Sonia DAndreta <sonia.dandreta@autotrader.co.uk>
2025-02-12 20:28:54 +0000

    Wrote test to populate a 2d array and method implementation

--- src/main/java/org/example/DataReverse.java
+++ src/main/java/org/example/DataReverse.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class DataReverse {
     public static int[] dataReverse(int[] data) {
+        //is next step to use converts method on a data array so we have an actual array to use?
+        //then
+
         return new int[0];
     }
 
@@ -17,6 +20,24 @@ public class DataReverse {
     }
 
     public static int[][] convertsTo2dArray(int[] data, int byteSize) {
-        return new int[data.length / byteSize][byteSize];
+        int[][] twoDArray = new int[data.length / byteSize][byteSize];
+
+        int arr = 0;
+
+        for (int outerIndex = 0; outerIndex < twoDArray.length; outerIndex++) {
+
+            //when 2nd outer loop starts it should be on index 8 but inner index has been reset to 0
+            for (int innerIndex = 0; innerIndex < twoDArray[outerIndex].length; innerIndex++) {
+                twoDArray[outerIndex][innerIndex] = data[arr];
+                arr += 1;//the data index can't be called innerIndex.
+            }
+            //at the end of 1st inner loop should have gone to 7th index of data array
+
+            //data[innerIndex] = data[innerIndex * outerIndex];
+            //data[outerIndex] += 1; this isn't incrementing the index. data[outerIndex += 1] increments the index.
+            //data = data[outerIndex += 1];
+
+        }
+        return twoDArray;
     }
 }
--- src/test/java/org/example/DataReverseTest.java
+++ src/test/java/org/example/DataReverseTest.java
@@ -38,4 +38,17 @@ class DataReverseTest {
         //then
         assertThat(result.length).isEqualTo(10);
     }
+
+    @Test
+    void convertsTo2dArray_populatesA2dArray() {
+        //given
+        int[] data = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+        int byteSize = 8;
+
+        //when
+        int[][] result = DataReverse.convertsTo2dArray(data, byteSize);
+
+        //then
+        assertThat(result).isDeepEqualTo(new int[][]{{1, 2, 3, 4, 5, 6, 7, 8}, {9, 10, 11, 12, 13, 14, 15, 16}});
+    }
 }
