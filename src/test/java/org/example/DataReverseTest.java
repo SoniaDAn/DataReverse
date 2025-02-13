ce6a46e5c8ffdef57125dadf08059f49783989c7
Sonia DAndreta <sonia.dandreta@autotrader.co.uk>
2025-02-13 13:35:10 +0000

    Wrote another test to reverse 3 segments in 2d array and store in an array. Using original method implementation.

--- src/test/java/org/example/DataReverseTest.java
+++ src/test/java/org/example/DataReverseTest.java
@@ -90,4 +90,17 @@ class DataReverseTest {
         //then
         assertThat(result).containsExactly(9, 10, 11, 12, 13, 14, 15, 16, 1, 2, 3, 4, 5, 6, 7, 8);
     }
+
+    @Test
+    void reversesOrderOfSegmentsIn2dArray_reversesPositionsOf3Arrays() {
+        //given
+        int[][] TwoDArray = {{1, 2, 3, 4, 5, 6, 7, 8}, {9, 10, 11, 12, 13, 14, 15, 16}, {17, 18, 19, 20, 21, 22, 23, 24}};
+        int byteSize = 8;
+
+        //when
+        int[] result = DataReverse.reversesOrderOfSegmentsIn2dArray(TwoDArray, byteSize);
+
+        //then
+        assertThat(result).containsExactly(17, 18, 19, 20, 21, 22, 23, 24, 9, 10, 11, 12, 13, 14, 15, 16, 1, 2, 3, 4, 5, 6, 7, 8);
+    }
 }
